b0606f4e5c102d1819f2c12955b882c2de25f187
john <jonimsfn@gmail.com>
2022-03-23 23:25:02 -0700

    all

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,5 @@
+#include "main.h>
+
+char *_strcat(char *dest, char *str)
+{
+	int srcSize = *deshello habelkajslfjsaljfkl;t
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+alx-low_level_programming
+0x06-pointers_arrays_strings
+
